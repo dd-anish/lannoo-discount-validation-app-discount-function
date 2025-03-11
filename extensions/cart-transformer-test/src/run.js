4da9550429fc83c299ab57a5e5860517e59dfcf3
dd-anish <anish@deardigital.com>
2025-03-11 17:25:29 +0530

    removed Product DIscount Extension and init Cart Transform Extension

---
+++ extensions/cart-transformer-test/src/run.js
@@ -0,0 +1,43 @@
+// @ts-check
+
+/**
+ * @typedef {import("../generated/api").RunInput} RunInput
+ * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ */
+
+/**
+ * @type {FunctionRunResult}
+ */
+
+const NO_CHANGES = {
+  operations: [],
+};
+
+/**
+ * @param {RunInput} input
+ * @returns {FunctionRunResult}
+*/
+export function run(input) {
+
+  console.log("✅ Cart Transformer Function is Running!");
+
+  return NO_CHANGES;
+};
+
+/* export default function run(input) {
+  const cart = input.cart;
+  console.log("🛒 Cart Transformer Function Triggered!", cart);
+
+  return {
+    operations: [
+      {
+        type: "update",
+        cartLines: cart.lines.map(line => ({
+          id: line.id,
+          quantity: 1, // Force quantity to 1 for testing
+        }))
+      }
+    ]
+  };
+}
+ */
