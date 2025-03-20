c62002b63711de665bb18ff58b68c7242e94ddf7
dd-anish <anish@deardigital.com>
2025-03-20 22:22:48 +0530

    Discount is applied based on Specific Product Tag and for the Specific Customer Tag Only

--- extensions/product-discount/src/run.js
+++ extensions/product-discount/src/run.js
@@ -20,17 +20,43 @@ const EMPTY_DISCOUNT = {
  */
 export function run(input) {
   
+  const customer = input.cart.buyerIdentity?.customer;
+
+  // Skip if no customer
+  if (!customer) {
+    return EMPTY_DISCOUNT;
+  }
+
   const targets = input.cart.lines
     .filter((line) => {
       if(line.merchandise.__typename === "ProductVariant"){
 
-        const isTaggedProduct = line.merchandise.product.hasAnyTag;
-        const isTaggedCustomer = input.cart.buyerIdentity?.customer?.hasAnyTag;
-        
-        const canCustomerOrderSample = input.cart.buyerIdentity?.customer?.metafield?.value === "true";
-        const productHasSampleCopy = line.merchandise.product.metafield?.value === "true";
 
-        return isTaggedProduct && isTaggedCustomer && canCustomerOrderSample && productHasSampleCopy;
+        const isTaggedProduct = line.merchandise.product.hasAnyTag;
+
+        // if Product has any one of the Tag:  ["teacher", "journalist", "influencer"]
+        if(isTaggedProduct){
+
+          const productTags = line.merchandise.product.hasTags || []
+          const customerTags = input.cart.buyerIdentity?.customer?.hasTags || []
+
+          // Extract tag names
+          const productTagNames = productTags.filter(({ hasTag }) => hasTag).map(({ tag }) => tag);
+          const customerTagNames = customerTags.filter(({ hasTag }) => hasTag).map(({ tag }) => tag);
+
+          const matchingTags = productTagNames.filter(tag => customerTagNames.includes(tag));
+
+          console.log("Product Tags: ", productTagNames);
+          console.log("Customer Tags: ", customerTagNames);
+          
+
+          console.log("Matching Tags:", matchingTags);
+
+          const canCustomerOrderSample = input.cart.buyerIdentity?.customer?.metafield?.value === "true";
+          const productHasSampleCopy = line.merchandise.product.metafield?.value === "true";
+
+          return productHasSampleCopy && canCustomerOrderSample && matchingTags.length > 0;
+        }
       }
       return false;
     })
@@ -43,9 +69,14 @@ export function run(input) {
       };
     });
 
-  console.log("Product Metafield:", JSON.stringify(input.cart.lines[0].merchandise.product.metafield, null, 2));
-  console.log("Buyer Identity:", JSON.stringify(input.cart.buyerIdentity, null, 2));
-  console.log("Targets:", JSON.stringify(targets, null, 2));
+  // console.log("Product Metafield:", JSON.stringify(input.cart.lines[0].merchandise.product.metafield, null, 2));
+  // console.log("Buyer Identity:", JSON.stringify(input.cart.buyerIdentity, null, 2));
+  // console.log("Targets:", JSON.stringify(targets, null, 2));
+  // console.log("Has Tags:", JSON.stringify(input.cart.buyerIdentity?.customer?.hasTags, null, 2));
+  
+  console.log("Customer Email: ", input.cart.buyerIdentity?.customer?.email);
+  console.log("Customer Can Order? : ", input.cart.buyerIdentity?.customer?.metafield?.value);
+  
 
   // If no targets, return empty discount
   if (targets.length === 0) {
@@ -68,7 +99,7 @@ export function run(input) {
     ],
   };
 
-  console.log("Discounted Items:", JSON.stringify(DISCOUNTED_ITEMS, null, 2));
+  // console.log("Discounted Items:", JSON.stringify(DISCOUNTED_ITEMS, null, 2));
 
   return DISCOUNTED_ITEMS;
 };
