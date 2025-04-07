d46298caced92a90778d36f9fe45b6f8c9f1f7f1
dd-anish <anish@deardigital.com>
2025-04-07 12:42:18 +0530

    Added UI Extension of Excluding Discounts to Selected Collection

--- extensions/product-discount-admin-ui/src/run.js
+++ extensions/product-discount-admin-ui/src/run.js
@@ -1,9 +1,12 @@
 // @ts-check
 import { DiscountApplicationStrategy } from "../generated/api";
 
+// Use JSDoc annotations for type safety
 /**
  * @typedef {import("../generated/api").RunInput} RunInput
  * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ * @typedef {import("../generated/api").Target} Target
+ * @typedef {import("../generated/api").ProductVariant} ProductVariant
  */
 
 /**
@@ -13,15 +16,59 @@ const EMPTY_DISCOUNT = {
   discountApplicationStrategy: DiscountApplicationStrategy.First,
   discounts: [],
 };
-
 /**
  * @param {RunInput} input
  * @returns {FunctionRunResult}
  */
 export function run(input) {
+  /**
+   * @type {{
+   *   collections: array
+   *   percentage: number
+   * }}
+   */
   const configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? "{}"
+    input?.discountNode?.metafield?.value ?? '{}',
   );
+  
+  if (!configuration.collections || !configuration.percentage) {
+    return EMPTY_DISCOUNT;
+  }
 
-  return EMPTY_DISCOUNT;
-};
+  const targets = input.cart.lines
+    .filter(line => {
+      if (line.merchandise.__typename == 'ProductVariant') {
+        const variant = /** @type {ProductVariant} */ (line.merchandise);
+        return !variant.product.inAnyCollection;
+      } else {
+        return false;
+      }
+    })
+    .map(line => {
+      const variant = /** @type {ProductVariant} */ (line.merchandise);
+
+      return /** @type {Target} */ ({
+        productVariant: {
+          id: variant.id,
+        },
+      });
+    });
+
+  if (!targets.length) {
+    return EMPTY_DISCOUNT;
+  }
+
+  return {
+    discounts: [
+      {
+        targets,
+        value: {
+          percentage: {
+            value: configuration.percentage.toString(),
+          },
+        },
+      },
+    ],
+    discountApplicationStrategy: DiscountApplicationStrategy.First,
+  };
+}
