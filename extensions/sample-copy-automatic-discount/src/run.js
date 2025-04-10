9976fc8840f430001d998fca9a30933a2779be78
dd-anish <anish@deardigital.com>
2025-04-10 17:35:14 +0530

    Edit according to Lannoo Store

--- extensions/sample-copy-automatic-discount/src/run.js
+++ extensions/sample-copy-automatic-discount/src/run.js
@@ -1,3 +1,4 @@
+// run.js
 // @ts-check
 import { DiscountApplicationStrategy } from "../generated/api";
 
@@ -11,7 +12,7 @@ import { DiscountApplicationStrategy } from "../generated/api";
  */
 const EMPTY_DISCOUNT = {
   discountApplicationStrategy: DiscountApplicationStrategy.First,
-  discounts: [],
+  discounts: []
 };
 
 /**
@@ -19,9 +20,80 @@ const EMPTY_DISCOUNT = {
  * @returns {FunctionRunResult}
  */
 export function run(input) {
-  const configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? "{}"
-  );
 
-  return EMPTY_DISCOUNT;
+  const customer = input.cart.buyerIdentity?.customer;
+
+  // Skip if no customer
+  if (!customer) {
+    console.log("No Customer LOGGED IN")
+    return EMPTY_DISCOUNT;
+  }
+
+  const canCustomerOrderSample = customer.canOrderSamples?.value === "true";
+  const customerSegment = customer.customerSegment?.value;
+  const claimedFreeSampleCopyProductIds = customer.claimedFreeSampleCopyProductIds?.jsonValue || [];
+
+  console.log("Customer Email:", customer.email);
+  console.log("Customer Can Order?:", canCustomerOrderSample);
+  console.log("Customer Name:", customer.displayName);
+  console.log("Customer Segment:", customerSegment);
+
+  console.log("Customer Product IDs", JSON.stringify(customer.claimedFreeSampleCopyProductIds, null, 2));
+
+  const targets = input.cart.lines
+    .filter((line) => {
+
+      if (line.merchandise.__typename !== "ProductVariant") {
+        return false;
+      }
+
+      const productSegment = line.merchandise?.product.productSegment?.value;
+
+      const matchingSegment = customerSegment === productSegment;
+
+      // The Product Metafield: has_sample_copy should be TRUE and mentioning === is must as without it the checking is not validated properly
+      const productHasSampleCopy = line.merchandise.product.hasSampleCopy?.value === 'true';
+
+      // The Customer should not have claimed the product before
+      const alreadyClaimedSampleCopy = claimedFreeSampleCopyProductIds.includes(line.merchandise.product.id);
+
+      // All Conditions for the Discounting a Product
+      const isProductEligibleForDiscount = productHasSampleCopy && canCustomerOrderSample && matchingSegment && !alreadyClaimedSampleCopy;
+
+      console.log("Discount on Product: ", isProductEligibleForDiscount);
+
+      return isProductEligibleForDiscount;
+
+    })
+    .map((line) => {
+      return {
+        productVariant: {
+          id: line.merchandise.id,
+          quantity: 1
+        }
+      };
+    });
+
+  // If no targets, return empty discount
+  if (targets.length === 0) {
+    return EMPTY_DISCOUNT;
+  }
+
+
+  const DISCOUNTED_ITEMS = {
+    discountApplicationStrategy: DiscountApplicationStrategy.First,
+    discounts: [
+      {
+        targets: targets,
+        value: {
+          percentage: {
+            value: 100,
+          }
+        },
+        message: `FREE SAMPLE COPY for ${input.cart.buyerIdentity?.customer?.displayName}`,
+      },
+    ],
+  };
+
+  return DISCOUNTED_ITEMS;
 };
