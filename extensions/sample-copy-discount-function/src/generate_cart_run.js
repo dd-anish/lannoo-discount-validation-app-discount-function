1475682eaa249bfb271e985b54d5c0bd7e48064e
dd-anish <anish@deardigital.com>
2025-04-15 12:22:26 +0530

    init Discount New API

---
+++ extensions/sample-copy-discount-function/src/generate_cart_run.js
@@ -0,0 +1,83 @@
+// @ts-check
+
+import {
+    OrderDiscountSelectionStrategy,
+    ProductDiscountSelectionStrategy,
+} from '../generated/api';
+
+/**
+ * @typedef {import("../generated/api").CartInput} CartInput
+ * @typedef {import("../generated/api").CartLinesDiscountsGenerateRunResult} CartLinesDiscountsGenerateRunResult
+ */
+
+/**
+ * generateCartRun
+ * @param {CartInput} input - The CartInput
+ * @returns {CartLinesDiscountsGenerateRunResult} - The function result with discounts.
+ */
+
+export function generateCartRun(input) {
+    if (!input.cart.lines.length) {
+        throw new Error('No cart lines found');
+    }
+
+    const maxCartLine = input.cart.lines.reduce((maxLine, line) => {
+        if (line.cost.subtotalAmount > maxLine.cost.subtotalAmount) {
+            console.log("Max Line:", line.cost.subtotalAmount.amount);
+
+            return line;
+        }
+        return maxLine;
+    }, input.cart.lines[0]);
+
+    // console.log("Max: ", JSON.stringify(maxCartLine, null, 2));
+
+    return {
+        operations: [
+            {
+                orderDiscountsAdd: {
+                    candidates: [
+                        {
+                            message: '10% OFF ORDER',
+                            targets: [
+                                {
+                                    orderSubtotal: {
+                                        excludedCartLineIds: [],
+                                    },
+                                },
+                            ],
+                            value: {
+                                percentage: {
+                                    value: 10,
+                                },
+                            },
+                        },
+                    ],
+                    selectionStrategy: OrderDiscountSelectionStrategy.First,
+                },
+            },
+            {
+                productDiscountsAdd: {
+                    candidates: [
+                        {
+                            message: '20% OFF PRODUCT',
+                            targets: [
+                                {
+                                    cartLine: {
+                                        id: maxCartLine.id,
+                                    },
+                                },
+                            ],
+                            value: {
+                                percentage: {
+                                    value: 20,
+                                },
+                            },
+                        },
+                    ],
+                    selectionStrategy: ProductDiscountSelectionStrategy.First,
+                },
+            },
+        ],
+    };
+}
---
+++ extensions/sample-copy-discount-function/src/generate_delivery_run.js
@@ -0,0 +1,50 @@
+// @ts-check
+
+import { DeliveryDiscountSelectionStrategy } from '../generated/api';
+
+/**
+ * @typedef {import("../generated/api").DeliveryInput} RunInput
+ * @typedef {import("../generated/api").CartDeliveryOptionsDiscountsGenerateRunResult} CartDeliveryOptionsDiscountsGenerateRunResult
+ */
+/**
+ * generateDeliveryRun
+ * @param {RunInput} input - The DeliveryInput
+ * @returns {CartDeliveryOptionsDiscountsGenerateRunResult} - The function result with discounts.
+ */
+export function generateDeliveryRun(input) {
+
+    const firstDeliveryGroup = input.cart.deliveryGroups[0];
+    if (!firstDeliveryGroup) {
+        throw new Error('No delivery groups found');
+    }
+
+    console.log("Delivery Groups", JSON.stringify(firstDeliveryGroup, null, 2));
+
+
+    return {
+        operations: [
+            {
+                deliveryDiscountsAdd: {
+                    candidates: [
+                        {
+                            message: 'FREE DELIVERY',
+                            targets: [
+                                {
+                                    deliveryGroup: {
+                                        id: firstDeliveryGroup.id,
+                                    },
+                                },
+                            ],
+                            value: {
+                                percentage: {
+                                    value: 100,
+                                },
+                            },
+                        },
+                    ],
+                    selectionStrategy: DeliveryDiscountSelectionStrategy.All,
+                },
+            },
+        ],
+    };
+}
